f19f2107de7fd328e437bc40928a0f68aaba1aa7
Tim Hopp <57064437+hoptimized@users.noreply.github.com>
2022-07-16 11:15:20 +0200

    Update build_web.bat

--- build_web.bat
+++ build_web.bat
@@ -60,7 +60,7 @@ IF %FAST% == 0 (
   echo Optimizing wasm...
   @REM to get wasm-opt:  apt/brew/dnf install binaryen
   @REM add -g to get debug symbols :
-  wasm-opt "docs\%CRATE_NAME%_bg.wasm" -O2 --fast-math -o "docs\%CRATE_NAME%_bg.wasm"
+  wasm-opt "docs\%CRATE_NAME%_bg.wasm" -O2 -o "docs\%CRATE_NAME%_bg.wasm"
 )
 
 echo Finished: docs/%CRATE_NAME_SNAKE_CASE%.wasm"
