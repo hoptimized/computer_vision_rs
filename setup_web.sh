8b3f15eb375d1dfdd204b06468b8c3109fe79e9a
Tim Hopp <57064437+hoptimized@users.noreply.github.com>
2022-07-28 21:01:10 +0200

    [CI/CD] Fix WASM32 deployment issue
    Fix an issue where the WASM32 deployment would fail even though all build 
    checks had passed.
    
    #Fix1
    Let the pipeline determine the correct wasm-bindgen version as defined in
    cargo.lock, and install build tools for the specified version.
    Deployment fails if the wasm-bindgen version used in the rust build differs 
    from the wasm-bindgen version used for deployment and wasm optimization.
    
    #Fix2
    Restructure pipeline to perform a full WASM32 build in the CI job, including 
    optimization of wasm output.
    
    Other improvements:
    - add `cancel-in-progress`
    - lint on all platforms 
    - cache wasm32 build for later deployment

--- setup_web.sh
+++ setup_web.sh
@@ -3,8 +3,7 @@ set -eu
 
 # Pre-requisites:
 rustup target add wasm32-unknown-unknown
-cargo install wasm-bindgen-cli
-cargo update -p wasm-bindgen
+cargo install wasm-bindgen-cli --version $(cargo pkgid wasm-bindgen | grep -Po '(?<=#wasm-bindgen[@,:])(.*)')
 
 # For local tests with `./start_server`:
 cargo install basic-http-server
