749dbffd1cd17a2bba17cb3f9f3aa3368e367cf1
hoptimized <57064437+hoptimized@users.noreply.github.com>
2022-08-05 16:22:55 +0200

    Switch to production

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
@@ -26,12 +25,6 @@ module.exports = {
                 },
             ]
         }),
-        // Have this example work in Edge which doesn't ship `TextEncoder` or
-        // `TextDecoder` at this time.
-        new webpack.ProvidePlugin({
-          TextDecoder: ['text-encoding', 'TextDecoder'],
-          TextEncoder: ['text-encoding', 'TextEncoder']
-        })
     ],
     devServer: {
         static: {
@@ -39,7 +32,7 @@ module.exports = {
         },
         port: 8080,
     },
-    mode: 'development',
+    mode: 'production',
     stats: 'minimal',
     experiments: {
         asyncWebAssembly: true
