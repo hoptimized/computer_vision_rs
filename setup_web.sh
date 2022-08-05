5cd7c009d72e5c264f6fe0e1cfcd1dee0b135b1e
Tim Hopp <57064437+hoptimized@users.noreply.github.com>
2022-08-05 17:10:00 +0200

    Switch to wasm-pack / webpack
    - adjust WASM32 build process and pipeline to use npm, wasm-pack and webpack
    - move all web files into dedicated folder `www`
    - use workbox automated service worker
    - minor updates to manifest.json

--- setup_web.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-
-# Pre-requisites:
-rustup target add wasm32-unknown-unknown
-cargo install wasm-bindgen-cli --version $(cargo pkgid wasm-bindgen | grep -Po '(?<=#wasm-bindgen[@,:])(.*)')
-
-# For local tests with `./start_server`:
-cargo install basic-http-server
---
+++ www/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, "index.js"),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js'
+    },
+    plugins: [
+        new WasmPackPlugin({
+            crateDirectory: path.resolve(__dirname, ".."),
+            outDir: path.resolve(__dirname, "pkg"),
+        }),
+        new WorkboxPlugin.GenerateSW({
+            clientsClaim: true,
+            skipWaiting: true,
+            maximumFileSizeToCacheInBytes: 10000000,
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "index.html"),
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "static"),
+                    to: path.resolve(__dirname, "dist")
+                },
+            ]
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        port: 8080,
+    },
+    mode: 'production',
+    stats: 'minimal',
+    experiments: {
+        asyncWebAssembly: true
+    }
+};
